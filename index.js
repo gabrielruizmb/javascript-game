7325dcd64762aa438c50d52b28ab802e2bf680c5
gabrielruizmb <gabrielruizmb@gmail.com>
2022-12-28 17:06:43 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,31 @@
+let moveArea = document.getElementById('move-area');
+
+let gameInstructions = document.createElement('p');
+gameInstructions.id = 'game-instructions';
+gameInstructions.innerHTML = `Use as setas direcionais do teclado para <br>
+se mover para esquerda e para direita!`
+moveArea.appendChild(gameInstructions);
+
+let char = document.createElement('div');
+char.id = 'char';
+char.innerHTML = `<img src="char.png">`
+moveArea.appendChild(char);
+
+let charPosition = 47;
+
+document.addEventListener('keydown', function(event){moveChar(event)});
+
+function moveChar(event) {
+  if (event.key == "ArrowLeft") {
+    if (charPosition > 1) {
+      charPosition --;
+      document.getElementById('char').style.left = `${charPosition}vw`;
+    }
+  }
+  if (event.key == "ArrowRight") {
+    if (charPosition < 93) {
+      charPosition ++;
+      document.getElementById('char').style.left = `${charPosition}vw`;
+    }
+  }
+}
