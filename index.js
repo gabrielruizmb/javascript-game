f06dc14fab89683217caa154c11b83fb62923a75
gabrielruizmb <gabrielruizmb@gmail.com>
2023-01-22 21:35:44 -0300

    adcionado item na tela

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ char.innerHTML = `<img src="char.png">`
 gameArea.appendChild(char);
 
 let charPosition = 380;
+let itemVerticalPosition = 55;
+let itemHorizontalPosition = 0;
 
 document.addEventListener('keydown', function(event){moveChar(event)});
 
@@ -30,13 +32,33 @@ function moveChar(event) {
   }
 }
 
+function spawnItem() {
+  document.getElementById('item').style.bottom = `${itemVerticalPosition}px`;
+  itemHorizontalPosition = Math.random() * 756;
+  document.getElementById('item').style.left = `${itemHorizontalPosition}px`;
+}
+
+function moveItem() {
+  if (itemVerticalPosition > -355) {
+    itemVerticalPosition -= 5;
+    document.getElementById('item').style.bottom = `${itemVerticalPosition}px`;
+
+    if (itemVerticalPosition == -355) {
+      itemVerticalPosition = 55;
+      spawnItem();
+    }
+  }
+}
+
 function createItem() {
   let item = document.createElement('div');
   item.id = 'item';
   item.innerHTML = `<img src="carrot.png">`;
   gameArea.appendChild(item);
 
-  document.getElementById('item').style.left = `${Math.random() * 756}px`;
+  spawnItem();
+  setInterval(moveItem, 100);
+
 }
 
 createItem();
